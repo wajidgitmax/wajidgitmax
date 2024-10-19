57592b1ea8b1dd03b442c6b3f3b4a07c38ec0145
wajidgitmax <wajidkhang3537@gmail.com>
2024-10-20 04:14:04 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/titan/titan.py",
+    "postStartCommand": "python3 /workspaces/wajidgitmax/titan.py",
     "customizations": {
         "vscode": {
             "settings": {
